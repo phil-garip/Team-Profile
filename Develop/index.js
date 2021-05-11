f932ceda1a6042cde06ee8ee471a5df954cde3c7
Philip Garip <phil.garip@gmail.com>
2021-05-11 15:21:26 -0400

    added html generate code

--- Develop/index.js
+++ Develop/index.js
@@ -78,3 +78,117 @@ function addMember() {
         });
     });
 }
+
+function startHtml() {
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+        <link rel="stylesheet" href="./style.css">
+    </head>
+    <body>
+        <div class="container main">
+            <div class="container-fluid head">
+                <h1>My Team</h1>
+            </div>
+        </div>
+        
+        `;
+    fs.writeFile("team.html", html, function(err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+    console.log("start");
+}
+
+function addHtml(member) {
+    return new Promise(function(resolve, reject) {
+        const name = member.getName();
+        const role = member.getRole();
+        const id = member.getId();
+        const email = member.getEmail();
+        let data = "";
+        if (role === "Engineer") {
+            const gitHub = member.getGithub();
+            data = `<div class="card" id="employee-card" style="width: 18rem;">
+            <div class="card-body">
+                <div class="container card-head">
+                    <h5 class="card-title" id="employee-name">${name}</h5>
+                    <h6 class="card-subtitle mb-2" id="employee-title">${role}</h6>
+                </div>
+                <div class="container card-body">
+                    <ul class="list-group card-list">
+                        <li class="list-group-item" id="id">ID: ${id} </li>
+                        <li class="list-group-item" id="id">Email: ${email}  </li>
+                        <li class="list-group-item" id="id">Github: ${gitHub} </li>
+                    </ul>
+                </div>
+            </div>
+        </div>`;
+        } else if (role === "Intern") {
+            const school = member.getSchool();
+            data = `<div class="card" id="employee-card" style="width: 18rem;">
+            <div class="card-body">
+                <div class="container card-head">
+                    <h5 class="card-title" id="employee-name">${name}</h5>
+                    <h6 class="card-subtitle mb-2" id="employee-title">${role}</h6>
+                </div>
+                <div class="container card-body">
+                    <ul class="list-group card-list">
+                        <li class="list-group-item" id="id">ID: ${id} </li>
+                        <li class="list-group-item" id="id">Email: ${email}  </li>
+                        <li class="list-group-item" id="id">School: ${school} </li>
+                    </ul>
+                </div>
+            </div>
+        </div>`;
+        } else {
+            const officePhone = member.getOfficeNumber();
+            data = `<div class="card" id="employee-card" style="width: 18rem;">
+            <div class="card-body">
+                <div class="container card-head">
+                    <h5 class="card-title" id="employee-name">${name}</h5>
+                    <h6 class="card-subtitle mb-2" id="employee-title">${role}</h6>
+                </div>
+                <div class="container card-body">
+                    <ul class="list-group card-list">
+                        <li class="list-group-item" id="id">ID: ${id} </li>
+                        <li class="list-group-item" id="id">Email: ${email}  </li>
+                        <li class="list-group-item" id="id">Office Number: ${officePhone} </li>
+                    </ul>
+                </div>
+            </div>
+        </div>`
+        }
+        console.log("adding team member");
+        fs.appendFile("team.html", data, function (err) {
+            if (err) {
+                return reject(err);
+            };
+            return resolve();
+        });
+    });
+    
+            
+    
+        
+    
+    
+}
+
+function finishHtml() {
+    const html = ` <script src="../index.js"></script>
+    </body>
+    </html>`;
+
+    fs.appendFile("./output/team.html", html, function (err) {
+        if (err) {
+            console.log(err);
+        };
+    });
+    console.log("end");
+}
